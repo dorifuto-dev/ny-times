bfda4c4f8b33373b3c8a71b0c748649ccd1ef23b
Eric Li <ericli@knights.ucf.edu>
2021-11-09 21:05:30 -0700

    Import fetching function and set article data to App component's state

---
+++ src/components/ArticleContainer/ArticleContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ArticleContainer = () => {
+  return (
+    <section className="article-container">
+      <p>ARTICLE CONTAINER</p>
+    </section>
+  );
+}
+
+export default ArticleContainer;
