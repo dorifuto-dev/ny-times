2ed68067e329fdadffc67326a4d29309303ebadd
Eric Li <ericli@knights.ucf.edu>
2021-11-10 00:55:49 -0700

    Add dayjs to dataCleaning file

--- src/dataCleaning.js
+++ src/dataCleaning.js
@@ -1,9 +1,11 @@
+import dayjs from 'dayjs'
+
 export const cleanArticleData = (data) => {
   return data.results.map(article => {
     return {
       id: article.id,
-      date: article.published_date,
-      updated: article.updated,
+      date: dayjs(article.published_date).format('MMMM D, YYYY'),
+      updated: dayjs(article.updated).format('MMMM D, YYYY'),
       byline: article.byline,
       title: article.title,
       content: article.abstract
