f0ea1cc4245ebdf49b7e2c0fe2646345114e5592
Eric Li <ericli@knights.ucf.edu>
2021-11-11 22:45:09 -0700

    Refactor article id and add url property

--- src/dataCleaning.js
+++ src/dataCleaning.js
@@ -3,12 +3,13 @@ import dayjs from 'dayjs'
 export const cleanArticleData = (data) => {
   return data.results.map(article => {
     return {
-      id: article.id,
+      id: article.uri.slice(-36),
       date: dayjs(article.published_date).format('MMMM D, YYYY'),
       updated: dayjs(article.updated).format('MMMM D, YYYY'),
       byline: article.byline,
       title: article.title,
-      content: article.abstract
+      content: article.abstract,
+      url: article.url
     }
   })
 }
